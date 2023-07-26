df87e6f25b6415d713492f58982060407ff809f8
gawrgu <gawrgura009@gmail.com>
2023-07-26 18:41:00 +0700

    create website caffe terrace

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle Active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Menghilangkan haburger menu di luar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
